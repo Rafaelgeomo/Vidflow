029ce4f39f8e7ebb16d8393f27e2c3800a14c5b1
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-10-27 12:14:58 -0300

    funcionabilidade barra de pesquisa

--- script.js
+++ script.js
@@ -26,4 +26,30 @@ async function buscarEMostrarVideos(){
     } 
 }
 
+
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector('.pesquisar__input')
+
+barraDePesquisa.addEventListener('input', filtarPesquisa)
+
+function filtarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item')
+
+    if(barraDePesquisa.value != "" ){
+        for(let video of videos) {
+            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";                
+            } else {
+                video.style.display = "block";
+            }
+        }
+    } else {
+        video.style.display = "block";
+    }
+
+}
+
