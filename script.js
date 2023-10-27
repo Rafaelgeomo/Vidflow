6841621ea7f0ee403ee6fce9f1e83b0b64788434
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-10-27 12:59:05 -0300

    filtrar por categoria

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function buscarEMostrarVideos(){
                         <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;
@@ -34,22 +35,35 @@ const barraDePesquisa = document.querySelector('.pesquisar__input')
 barraDePesquisa.addEventListener('input', filtarPesquisa)
 
 function filtarPesquisa() {
-    const videos = document.querySelectorAll('.videos__item')
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
 
-    if(barraDePesquisa.value != "" ){
-        for(let video of videos) {
-            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
-            let valorFiltro = barraDePesquisa.value.toLowerCase();
+    videos.forEach ((video)=> {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
 
-            if(!titulo.includes(valorFiltro)){
-                video.style.display = "none";                
-            } else {
-                video.style.display = "block";
-            }
-        }
-    } else {
-        video.style.display = "block";
-    }
-
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });  
 }
+ 
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
+botaoCategoria.forEach((botao)=>{
+    let nomeCategoria = botao.getAttribute('name');
+    botao.addEventListener('click', () => filtarPorCategoria(nomeCategoria));    
+})
+
+function filtarPorCategoria(filtro) {
+    const videos = document.querySelectorAll('.videos__item');
+    for(let video of videos){
+        let categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
+
 
